04009635ca918b91c0f3bae2e73b94f2f8a9ab06
Summers-Rain <Dguilliams99@gmail.com>
2025-04-15 03:46:36 -0500

    Added OOP

--- game.py
+++ game.py
@@ -1,19 +1,23 @@
 import sys
 import pygame
 
-pygame.init()
+class Game:
+    def __init__(self):
+        pygame.init()
 
-pygame.display.set_caption("Ninja Game")
-screen = pygame.display.set_mode((640, 480))
+        pygame.display.set_caption("Ninja Game")
+        self.screen = pygame.display.set_mode((640, 480))
 
-clock = pygame.time.Clock()
+        self.clock = pygame.time.Clock()
 
-while True:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            pygame.quit()
-            sys.exit
+    def run(self):
+        while True:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    sys.exit()
 
-    pygame.display.update()
-    #force loop to run at 60 fps
-    clock.tick(60)
+            pygame.display.update()
+            #force loop to run at 60 fps
+            self.clock.tick(60)
+Game().run()
