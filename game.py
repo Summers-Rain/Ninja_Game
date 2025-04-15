ceaa372f3f30e450c7395481c3567203c63bd003
Summers-Rain <Dguilliams99@gmail.com>
2025-04-15 04:07:59 -0500

    added a cloud that you can move up and down with arrow keys, also added a sky blue background

--- game.py
+++ game.py
@@ -10,13 +10,34 @@ class Game:
 
         self.clock = pygame.time.Clock()
 
+        self.img = pygame.image.load("data/images/clouds/cloud_1.png")
+        self.img.set_colorkey((0, 0, 0))
+
+        self.img_pos = [160, 260]
+        self.movement = [False, False]
+
     def run(self):
         while True:
+            self.screen.fill((14, 219, 248))
+
+            self.img_pos[1] += (self.movement[1] - self.movement[0]) * 5
+            self.screen.blit(self.img, self.img_pos)
+
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     pygame.quit()
                     sys.exit()
-
+                if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_UP:
+                        self.movement[0] = True
+                    if event.key == pygame.K_DOWN:
+                        self.movement[1] = True
+                if event.type == pygame.KEYUP:
+                    if event.key == pygame.K_UP:
+                        self.movement[0] = False
+                    if event.key == pygame.K_DOWN:
+                        self.movement[1] = False
+                
             pygame.display.update()
             #force loop to run at 60 fps
             self.clock.tick(60)
