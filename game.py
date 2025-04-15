68e51d63df29ea3f42f3949e1b8a21ceeca849f2
Summers-Rain <Dguilliams99@gmail.com>
2025-04-15 04:29:14 -0500

    added a blue box that lights up when you touch it, changed movement from Arrow Keys to W and S

--- game.py
+++ game.py
@@ -16,9 +16,17 @@ class Game:
         self.img_pos = [160, 260]
         self.movement = [False, False]
 
+        self.collision_area = pygame.Rect(50, 50, 300, 50)
+
     def run(self):
         while True:
             self.screen.fill((14, 219, 248))
+
+            img_r = pygame.Rect(self.img_pos[0], self.img_pos[1], self.img.get_width(), self.img.get_height())
+            if img_r.colliderect(self.collision_area):
+                pygame.draw.rect(self.screen, (0, 100, 255), self.collision_area)
+            else:
+                pygame.draw.rect(self.screen, (0, 50, 155), self.collision_area)
 
             self.img_pos[1] += (self.movement[1] - self.movement[0]) * 5
             self.screen.blit(self.img, self.img_pos)
@@ -28,14 +36,15 @@ class Game:
                     pygame.quit()
                     sys.exit()
                 if event.type == pygame.KEYDOWN:
-                    if event.key == pygame.K_UP:
+                    if event.key == pygame.K_w:
                         self.movement[0] = True
-                    if event.key == pygame.K_DOWN:
+                    if event.key == pygame.K_s:
                         self.movement[1] = True
+                    
                 if event.type == pygame.KEYUP:
-                    if event.key == pygame.K_UP:
+                    if event.key == pygame.K_w:
                         self.movement[0] = False
-                    if event.key == pygame.K_DOWN:
+                    if event.key == pygame.K_s:
                         self.movement[1] = False
                 
             pygame.display.update()
